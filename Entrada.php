0277c42e0adc9a2d051a2dbc433f18760da8cc0a
deivis.martinez <deivismartinez77@gmail.com>
2024-06-05 11:03:20 -0500

    Cambio acceso

--- Entrada.php
+++ Entrada.php
@@ -28,7 +28,7 @@
             <div class="col-xs-12">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Inscripción Docente</h3>
+                        <h3 class="panel-title">Inscripción Docente Prueba</h3>
                     </div>
                     <div class="panel-body">
                         <div>
