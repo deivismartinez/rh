7f4e29f9fb9da3b9e3617e9845ff69abb0a7a629
deivis.martinez <deivismartinez77@gmail.com>
2024-06-05 14:16:46 -0500

    Cambio acceso

--- Admin/NuevoPrograma.php
+++ Admin/NuevoPrograma.php
@@ -145,18 +145,14 @@ if (isset($_SESSION['usuario'])) {
                                         <div class="row">
                                             <div class="col-xs-6">
                                                 <label for="telefono">Facultad</label>
-                                                <select class="form-control" id="facultadCmb" name="facultadCmb" required="true" onchange=<?php
-                                                    ?>>
-                                                        <option value="">SELECCIONE</option>
+                                                <select class="form-control" id="facultadCmb" name="facultadCmb" required="true" onchange="">                                                      <option value="">SELECCIONE</option>
                                                         <?php
                                                         $facultades = $p->getFacultadesDocentePostgrado();
+                                                         foreach ($facultades as $arregloFac) {
                                                             echo '<OPTION value="' . $arregloFac[0] . '">' . $arregloFac[1] . '</OPTION>';
                                                         }
                                                         ?>
                                                     </select>
-                                                        <?php
-                                                        ?>
-
                                             </div>
                                             <div class="col-xs-6">
                                                 <label for="telefono">Nombre del nuevo Departamento</label>
