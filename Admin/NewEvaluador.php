f5dd9ef7dab68487b9f2a59bba4a141fd28a9644
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2024-09-12 11:35:46 -0500

    Update new Evaluador

--- Admin/NewEvaluador.php
+++ Admin/NewEvaluador.php
@@ -243,6 +243,12 @@ if (isset($usuario)) {
                                     </tbody>
                                 </table>
                                 </div>
+                                <div class="row">
+                                 <div class="col-xs-12">
+                                            <br>
+                                            <input type="submit" value="Guardar" class="btn btn-primary" />
+                                        </div>
+                                    </div>
                             </div>
 
 
