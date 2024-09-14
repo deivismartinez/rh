3a085cf3a1b60a8db5fba0781e7a1777386b1895
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2024-09-14 06:53:54 -0500

    Create verificar_usuario.php

---
+++ Admin/verificar_usuario.php
@@ -0,0 +1,34 @@
+<?php
+require_once("../Tablero/vo/UsuarioVO.php");
+require_once("../Tablero/clases/Programas.php");
+require_once("../Tablero/clases/Gestion.php");
+
+session_start();
+$usuario = $_SESSION['usuario'];
+$archivo = "hvd".$usuario->getId();
+$_SESSION['id_usuario']=$usuario->getId();
+ $programa = new Programas();       
+ $usuarioEvaluador= $programa->getEvaluador(); 
+
+ $usuarioTxt= strtoupper($_POST["usuarioTxt"]);
+
+   
+
+
+// Obtener el nombre de usuario desde la petición AJAX
+$nombre_usuario = isset($_POST['usuarioTxt']) ? $_POST['usuarioTxt'] : '';
+
+//$existe= $programa->existeUsuario($usuarioTxt);
+
+if ($programa->existeUsuario($nombre_usuario)) {
+    echo json_encode(['existe' => true, 'mensaje' => 'El usuario ya existe.']);
+} else {
+    echo json_encode(['existe' => false, 'mensaje' => 'El usuario está disponible.']);
+}
+?>
+
+
+
+
+
+
