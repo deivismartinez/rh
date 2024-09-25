01211ddb7813f0eac2954c034a12f65640372dee
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2024-09-25 11:58:55 -0500

    Update EditEvaluador.php

--- Admin/EditEvaluador.php
+++ Admin/EditEvaluador.php
@@ -67,6 +67,7 @@ $opcionTipo = [
     <link href="../Tablero/assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet" />
     <link href="../Tablero/assets/css/demo.css" rel="stylesheet" />
     <link href="../Tablero/assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <style>
         body {
