acb00f5b806d73f71186d01ace68664a2570aa63
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2024-09-28 14:05:26 -0500

    Update funciones.js

--- Tablero/js/funciones.js
+++ Tablero/js/funciones.js
@@ -89,7 +89,7 @@ function cargarAreas(dep) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("comboAreas").innerHTML = this.responseText;
+           // document.getElementById("comboAreas").innerHTML = this.responseText;
         }
     };
     xhttp.open("GET", "AreasList.php?dep=" + dep, true);
