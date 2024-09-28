0faed1ea52a62b59d6485ea4e802f3aa628808ae
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2024-09-28 13:59:42 -0500

    actualizar

--- Tablero/newProgramasListSinAreas.php
+++ Tablero/newProgramasListSinAreas.php
@@ -1,4 +1,4 @@
-<select required="true" name="programaCmb" id="programaCmb" class="form-control" onchange="">
+<select required="true" name="programaCmb" id="programaCmb" class="form-control" onchange="cargarAreas(this.value)">
     <option value="">SELECCIONE</option>
     <?php
     require_once("clases/Programas.php");
