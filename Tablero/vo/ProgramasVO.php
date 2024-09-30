ba93b91b2d4b9a0de4d4d351bddf929babaec0bc
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2024-09-30 16:17:25 -0500

    buscando error

--- Tablero/vo/ProgramasVO.php
+++ Tablero/vo/ProgramasVO.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 class ProgramasVO 
@@ -24,7 +26,7 @@ class ProgramasVO
     }
     public function setId($id)
     {
-        $this->name = $id;
+        $this->id = $id;
     }
 
     public function getName()
