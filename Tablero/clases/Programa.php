323d55e6ecb42c1336fdccb06eeeb9bbf945612a
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2024-09-30 16:28:59 -0500

    actualizaciones

--- Tablero/clases/Programa.php
+++ Tablero/clases/Programa.php
@@ -1,10 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 require_once("conectar.php");
 require_once("helpers.php");
-require_once("../vo/ProgrmasVO.php");
+require_once("../vo/ProgramasVO.php");
 
 
 class Programa extends conectar
