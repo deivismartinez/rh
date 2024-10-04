9ac16ca200de95d6b6cd160e52f6354923fa5813
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2024-10-04 17:29:22 -0500

    Corrección SQL

--- Admin/procesar_filtro_facultad.php
+++ Admin/procesar_filtro_facultad.php
@@ -7,7 +7,7 @@ $db = $conexion->conectar(); // Obtener la conexión
 $facultadTxt = $_POST['facultadTxt'];
 
 // Modificar la consulta para agregar el filtro del nombre
-$sql = "SELECT nombre,estado,CASE WHEN posgrado = 't' THEN 'Posgrado' WHEN posgrado = 'f' THEN 'Pregrado' END,id FROM facultad  order by id desc
+$sql = "SELECT ROW_NUMBER() OVER () AS row_number, nombre, estado, CASE WHEN posgrado = 't' THEN 'Posgrado' WHEN posgrado = 'f' THEN 'Pregrado' END,id FROM facultad  order by id desc
         WHERE u.nombre ILIKE '%$facultadTxt%'";
 
 $datos = pg_query($db, $sql);
