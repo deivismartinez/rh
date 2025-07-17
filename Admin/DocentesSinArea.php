d65f78d9114ddc3abd202ce314801ced847148ac
ingsaumar <160366675+ingsaumar@users.noreply.github.com>
2025-07-17 15:29:23 -0500

    Update DocentesSinArea.php
    probando boostrap

--- Admin/DocentesSinArea.php
+++ Admin/DocentesSinArea.php
@@ -28,6 +28,7 @@ $url = "includes/".$usuario->getTipo().".php";
         <link href="../Tablero/assets/css/demo.css" rel="stylesheet" />
         <link href="../Tablero/assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
         <link href="../Tablero/assets/css/local.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     </head>
     <body>
         <div class="wrapper">
