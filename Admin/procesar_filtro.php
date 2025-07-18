8f9d12808307bed09459eb5fd6dc2051e7a07e3a
deivismartinez <deivism@uninorte.edu.co>
2025-07-18 17:36:08 -0500

    filters on admin

--- Admin/procesar_filtro.php
+++ Admin/procesar_filtro.php
@@ -12,13 +12,15 @@ $usuarioTxt = $_POST['usuarioTxt'];
 
 if($facultadCmb !=''){$facultadCmb =' and f.id = '.$facultadCmb;}
 if($programaCmb !=''){$programaCmb =' and p.id = '.$programaCmb;}
+if($rolCmb ==''){$rolCmb ='%';}
+if($sedeCmb ==''){$sedeCmb ='%';}
 
 // Modificar la consulta para agregar el filtro del nombre
 $sql = "SELECT ROW_NUMBER() OVER () AS row_number, u.nombre as nombre, u.correo as correo, p.nombre as nombre_programa, f.nombre as nombre_facultad, u.tipo as tipo, u.estado as estado, u.sede as sede, u.id 
         FROM usuario as u 
         INNER JOIN programa as p ON p.id = u.facultad_id 
         INNER JOIN facultad as f ON f.id = p.facultad_id 
-        WHERE u.nombre ILIKE '%$nombreCompletoTxt%' $facultadCmb $programaCmb";
+        WHERE u.sede ILIKE '%$sedeCmb%' and u.tipo ILIKE '%$rolCmb%' and u.correo ILIKE '%$usuarioTxt%' and u.nombre ILIKE '%$nombreCompletoTxt%' $facultadCmb $programaCmb";
 var_dump($sql);
 $datos = pg_query($db, $sql);
 
