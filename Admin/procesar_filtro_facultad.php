a9267c633b4f081360da9d6453d7a5dac1d287b8
deivismartinez <deivism@uninorte.edu.co>
2025-07-18 12:23:03 -0500

    Sizes icons

--- Admin/procesar_filtro_facultad.php
+++ Admin/procesar_filtro_facultad.php
@@ -12,7 +12,7 @@ if($alcanceCmb=='false'){$alcanceCmbI='f';}else{if($alcanceCmb=='true'){$alcance
 // Modificar la consulta para agregar el filtro del nombre
 $sql = "SELECT ROW_NUMBER() OVER () AS row_number, nombre,
     estado,CASE WHEN posgrado = 't' THEN 'Posgrado' WHEN posgrado = 'f' THEN 'Pregrado' END as posgrado,id FROM facultad
-        WHERE nombre ILIKE '%$facultadTxt%' and posgrado ILIKE '$alcanceCmbI'";
+        WHERE nombre ILIKE '%$facultadTxt%' and posgrado ILIKE '%$alcanceCmbI%'";
 
 $datos = pg_query($db, $sql);
 
