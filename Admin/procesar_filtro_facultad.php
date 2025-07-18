7bc8d36ab2f53ddae3ea534e64c5f4ea42ffe04c
deivismartinez <deivism@uninorte.edu.co>
2025-07-18 12:21:35 -0500

    Sizes icons

--- Admin/procesar_filtro_facultad.php
+++ Admin/procesar_filtro_facultad.php
@@ -14,7 +14,6 @@ $sql = "SELECT ROW_NUMBER() OVER () AS row_number, nombre,
     estado,CASE WHEN posgrado = 't' THEN 'Posgrado' WHEN posgrado = 'f' THEN 'Pregrado' END as posgrado,id FROM facultad
         WHERE nombre ILIKE '%$facultadTxt%' and posgrado ILIKE '$alcanceCmbI'";
 
-var_dump($sql);
 $datos = pg_query($db, $sql);
 
 // Generar la tabla con los resultados
